dff5cf185f814d0771a7c60d03295884bfc8fa4b
Polina Ganina <poliganina@gmail.com>
2022-08-27 14:58:01 +0300

    actions test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ImgUrlProvider } from "./features/kitty/context/imgUrlProvider";
 import { LoginProvider } from "./features/login/context/loginProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
